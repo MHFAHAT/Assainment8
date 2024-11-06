02ce184dd37811b8a6fba65a2234d49cae951b04
MHFahat <m.h.fahat20@gmail.com>
2024-11-06 21:56:45 +0600

    first commit

---
+++ src/Component/Common/Common.jsx
@@ -0,0 +1,16 @@
+import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/Navbar";
+
+ 
+
+const Common = () => {
+    return (
+        <div>
+             <Navbar></Navbar>
+             <div className="min-h-screen"></div>
+             <Footer></Footer>
+        </div>
+    );
+};
+
+export default Common;
